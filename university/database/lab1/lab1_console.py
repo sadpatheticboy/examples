ff5c2b3f146d95079d8daa1dd0cf23883577e825
sadpatheticboy <bal-tim@mail.ru>
2023-02-20 14:26:38 +0300

    new_lab1_console

--- university/database/lab1/lab1_console.py
+++ university/database/lab1/lab1_console.py
@@ -3,7 +3,7 @@ lst = [int(input('Элемент массива:')) for i in range(int(input('К
 count = sum([1 for i in lst if i < 0])
 
 c, d = int(input('Левая граница отрезка:')), int(input('Правая граница отрезка:'))
-new_lst = [lst.index(i) for i in lst if c < i < d]
+new_lst = [i for i in range(len(lst)) if c <= lst[i] <= d]
 
 print(f'Исходный массив: {lst}')
 print(f'Количество отрицательных элементов в исходном массиве: {count}')
