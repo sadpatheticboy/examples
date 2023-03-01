f9c31106d45f36327a95fa021662852f32e3e929
sadpatheticboy <bal-tim@mail.ru>
2023-03-01 23:12:44 +0300

    new_lessons

---
+++ stepik/dobryj_python_OOP/2.3.10.py
@@ -0,0 +1,28 @@
+""" Условие задачи - https://stepik.org/lesson/701985/step/10?unit=702086 """
+
+
+class Thing:
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+
+
+class Bag:
+    def __init__(self, max_weight):
+        self.max_weight = max_weight
+        self.__things = []
+
+    @property
+    def things(self):
+        return self.__things
+
+    def add_thing(self, thing):
+        s = self.get_total_weight()
+        if s + thing.weight <= self.max_weight:
+            self.__things.append(thing)
+
+    def remove_thing(self, indx):
+        self.__things.pop(indx)
+
+    def get_total_weight(self):
+        return sum(t.weight for t in self.__things)
