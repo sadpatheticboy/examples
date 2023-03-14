6f8baa30efb8bcc3fb3268b227cc4c093b61f33c
sadpatheticboy <bal-tim@mail.ru>
2023-03-14 21:40:36 +0300

    dan_bader_introduction_cpater_10

---
+++ books/dan_bader_introduction/chapter_10/10.3.2.py
@@ -0,0 +1,15 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+class Square(Rectangle):
+    def __init__(self, side_length):
+        super().__init__(side_length, side_length)
+
+sq = Square(4)
+print(sq.area())
