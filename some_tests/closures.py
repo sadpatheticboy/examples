6241051563a6226632eecfb8216f4f01b1a67306
sadpatheticboy <bal-tim@mail.ru>
2023-04-24 22:25:25 +0300

    test_closures

--- some_tests/closures.py
+++ some_tests/closures.py
@@ -10,8 +10,57 @@ def names():
     return inner
 
 
+def average():
+    values = []
+
+    def inner(value: int) -> float:
+        values.append(value)
+        return sum(values) / len(values)
+
+    return inner
+
+
+def counter():
+    count = 0
+
+    def inner(value: int) -> int:
+        nonlocal count
+        count += value
+        return count
+
+    return inner
+
+
+def pow_(base):
+    return lambda value: value ** base
+
+
 if __name__ == '__main__':
-    students = names()
-    print(students('Vasya'))
-    print(students('Petya'))
-    print(students('Dima'))
+    pow__ = pow_(2)
+    pow_2 = pow_(3)
+    print(pow__(5))
+    print(pow__(6))
+    print(pow__(7))
+    print(pow_2(5))
+    print(pow_2(6))
+    print(pow_2(7))
+
+    # count = counter()
+    # print(count(1))
+    # print(count(1))
+    # print(count(1))
+    # print(count(-3))
+
+    # avg = average()
+    # print(avg(10))
+    # print(avg(20))
+    # print(avg(30))
+
+    # boys = names()
+    # girls = names()
+    # print(boys('Vasya'))
+    # print(boys('Petya'))
+    # print(boys('Dima'))
+    # print(girls('Tanya'))
+    # print(girls('Sonya'))
+    # print(girls('Katya'))
