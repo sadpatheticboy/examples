3f044bf5086d3eb0fb365fbd14421668c0963e16
sadpatheticboy <bal-tim@mail.ru>
2023-05-15 00:00:13 +0300

    new_tests_calculator_pytest

---
+++ tests/test_calculator_pytest.py
@@ -0,0 +1,23 @@
+import pytest
+
+from some_tests.calculator import calculator
+
+
+def test_plus():
+    assert calculator('2+2') == 4
+
+
+def test_no_signs():
+    with pytest.raises(ValueError) as error:
+        calculator('abracadabra')
+    assert 'Выражение не содержит нужные арифмитеские операции: +-*/' == error.value.args[0]
+
+
+def test_two_signs():
+    with pytest.raises(ValueError) as error:
+        calculator('2+2+2')
+    assert 'Выражение должно содержать два целых числа и одну арифмитическую операцию' == error.value.args[0]
+
+
+if __name__ == '__main__':
+    pytest.main()
