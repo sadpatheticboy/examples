9e438acef05d1d867ead6e2aaee4be2a0a0a9eb1
sadpatheticboy <bal-tim@mail.ru>
2023-05-27 23:26:42 +0300

    Тест модуля multiprocessing

--- multiprocessing/code1.py
+++ multiprocessing/code1.py
@@ -1,5 +1,24 @@
+# В Python для создания процессов и работы с ними используется модуль multiprocessing
+import time
+from multiprocessing import Process
 import os
-from time import sleep
 
-print(f'Начинаю работу в процессе {os.getpid()}')  # Получение id процесса
+CNT = 0
 
+
+def f():
+    global CNT
+    print(f"f started from", os.getpid())
+    time.sleep(3)
+    CNT += 2
+    print(f"f: {CNT} from", os.getpid())
+
+
+if __name__ == ' __main__':
+    p = Process(target=f, args=())
+    p_1 = Process(target=f, args=())
+    p_2 = Process(target=f, args=())
+    p.start()
+    p_1.start()
+    p_2.start()
+    print("Finished!")
