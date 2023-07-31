9ac609251c1dca3c2c924c5dfdff06aaa60fa5bb
sadpatheticboy <bal-tim@mail.ru>
2023-07-31 17:54:17 +0300

    Работа со slice объектами

---
+++ something/slice.py
@@ -0,0 +1,9 @@
+text_1 = 'День чудесный!'
+text_2 = 'День грустняк!'
+
+slice_ = slice(5, -1)
+print(slice_)
+print(text_1[slice_])
+
+slice_reverse = slice(None, None, -1)
+print(text_1[slice_reverse])
